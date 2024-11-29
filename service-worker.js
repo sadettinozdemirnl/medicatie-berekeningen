2067eef5f62da2c11da1ed7cfdbd0d3aa7f51626
Sadettin Ozdemir <51020774+sadettinozdemirnl@users.noreply.github.com>
2024-11-29 16:45:34 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const cacheName = 'medicatie-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install the service worker and cache the files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+// Fetch files from the cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
